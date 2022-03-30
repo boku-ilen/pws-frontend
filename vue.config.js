74f5810bcb8c3c5a7854096145be1458c33dde1c
unknown <hiasbaum@gmail.com>
2022-03-30 17:03:22 +0200

    Updated proxy config so urls are not required in the script

--- vue.config.js
+++ vue.config.js
@@ -3,6 +3,11 @@ module.exports = defineConfig({
   transpileDependencies: true,
   configureWebpack: {
     devServer: {
+      proxy: {
+        "/tables": {
+          target: "http://192.168.0.165:8000"
+        }
+      },
       headers: { "Access-Control-Allow-Origin": "*" }
     }
   }
