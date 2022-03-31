191ed21d281e329e1471899b1c3a69988f839953
Karl <karl.bittner@hexaquo.at>
2022-03-31 14:11:46 +0200

    Added bootstrapvue

--- src/main.js
+++ src/main.js
@@ -1,5 +1,17 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import BootstrapVue3 from 'bootstrap-vue-3'
 
-createApp(App).use(router).mount('#app')
+// Since every component imports their Bootstrap functionality,
+// the following line is not necessary:
+// import 'bootstrap'
+
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue-3/dist/bootstrap-vue-3.css'
+
+
+createApp(App)
+    .use(router)
+    .use(BootstrapVue3)
+    .mount('#app')
