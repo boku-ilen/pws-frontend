4c5158b7bdf212c8de1408117669d0b2f3845c56
Karl <karl.bittner@hexaquo.at>
2022-03-31 11:04:27 +0200

    Created a small overview-intro

--- src/components/OverviewIntro.vue
+++ src/components/OverviewIntro.vue
@@ -1,32 +1,43 @@
 <template>
-    <mdb-container>
-    <mdb-row>
-      <mdb-col col="9">
-          <h1> PWS! Grüßgott - grüß Sie herzlich </h1>
-      </mdb-col>
-      <mdb-col>
-          <img src="../assets/pws.webp" class="img-fluid" alt="Responsive image">
-      </mdb-col>
-    </mdb-row>
-    <mdb-row>
-      <mdb-col>1 of 3</mdb-col>
-      <mdb-col col="5">2 of 3 (wider)</mdb-col>
-      <mdb-col>3 of 3</mdb-col>
-    </mdb-row>
-  </mdb-container>
+  <h1>Park Working Spaces</h1>
+  <div class="row">
+    <div class="col-2"></div>
+    <div class="col-6">
+      <p>
+        Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy
+        eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam
+        voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet
+        clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit
+        amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
+        nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat,
+        sed diam voluptua. At vero eos et accusam et justo duo dolores et ea
+        rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem
+        ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing
+        elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna
+        aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo
+        dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus
+        est Lorem ipsum dolor sit amet. Duis autem vel eum iriure dolor in
+        hendrerit in vulputate velit esse molestie consequat, vel illum dolore
+        eu feugiat nulla facilisis at vero eros et accumsan et iusto odio
+        dignissim qui blandit praesent luptatum zzril delenit augue duis dolore
+        te feugait nulla facilisi. Lorem ipsum dolor sit amet,
+      </p>
+    </div>
+    <div class="col-2">
+      <img src="../assets/pws.webp" height="200" alt="Responsive image" />
+    </div>
+  </div>
 </template>
 
 <script>
-  import {mdbContainer, mdbRow, mdbCol} from 'mdbvue';
-  export default {
-    components: {
-      mdbContainer,
-      mdbRow,
-      mdbCol
-    }
-  }
+import App from "@/App.vue";
+export default {
+  components: { App },
+};
 </script>
 
 <style>
-
+p {
+  text-align: left;
+}
 </style>
